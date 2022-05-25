dfb656f3810b4342ed9971910b95d7f5364a1299
unknown <Doge2Dev>
2022-05-25 17:38:39 -0300

    .chan changed and working

--- src/native/engine/chan/decode.lua
+++ src/native/engine/chan/decode.lua
@@ -3,6 +3,10 @@ chan_decode = {VERSION = "0.0.1"}
 stringx = require 'pl/stringx'
 
 function chan_decode.decode(filepath, chunkName)
+	return read(filepath, chunkName)
+end
+
+function read(filepath, chunkName, returnID)
 	i = 0
 	file = io.open(filepath .. ".chan", "r")
 	assert(file ~= nil, "[:ERROR:] | File not found, may is corrupted, moved or deleted.")
@@ -11,29 +15,23 @@ function chan_decode.decode(filepath, chunkName)
 		i = i + 1                --debug stuff
 		if string.find(line, chunkName) then
 			outStr = stringx.split(line, "::")
-			return outStr[2]
+			return outStr[returnID]
 		end
 	end
 end
 
-function valueExist(filepath, chunkName)
-	j = 0
-	file = io.open(filepath .. ".chan", "r")
-	lines = file:lines()
-	for line in lines do
-		j = j + 1
-		stringContent = string.find(line, chunkName)
-		if stringContent  then	--chekc if not null <3
-			return true
-		else
-			return false
-		end
+function checkValueExist(filepath, chunkName)
+	content = read(filepath, chunkName, 1)
+	if content == chunkName then
+		return true
+	else
+		return false
 	end
 end
 
 function chan_decode.append(filepath, chunkName, value)
+	boolValueExist = checkValueExist(filepath, chunkName)
 	file = io.open(filepath .. ".chan", "a")
-	boolValueExist = valueExist(filepath, chunkName)
 	print(boolValueExist)
 	if not boolValueExist or boolValueExist == nil then
 		file:write(chunkName .. "::" .. value .. "\n")
