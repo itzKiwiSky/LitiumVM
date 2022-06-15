d2ae3fe658e01de94a6b9fb0ed5e8cf9038297d5
unknown <Doge2Dev>
2022-06-14 21:27:38 -0300

    locked

--- src/native/engine/core/nativelocks.lua
+++ src/native/engine/core/nativelocks.lua
@@ -41,8 +41,42 @@ function nativelocks.lock()
     love.graphics.newVideo = false
     love.graphics.newVolumeImage = false
     love.graphics.setNewFont = false
+
+    -- Audio --
+
+    love.audio.getActiveEffects = false
+    love.audio.getActiveSourceCount = false
+    love.audio.getDistanceModel = false
+    love.audio.getDopplerScale = false
+    love.audio.getEffect = false
+    love.audio.getMaxSceneEffects = false
+    love.audio.getMaxSourceEffects = false
+    love.audio.getOrientation = false
+    love.audio.getPosition = false
+    love.audio.getRecordingDevices = false
+    love.audio.getVelocity = false
+    love.audio.getVolume = false
+    love.audio.isEffectsSupported = false
+    love.audio.newQueueableSource = false
+    love.audio.newSource = false
+    love.audio.pause = false
+    love.audio.play = false
+    love.audio.setActiveEffects = false
+    love.audio.setActiveSourceCount = false
+    love.audio.setDistanceModel = false
+    love.audio.setDopplerScale = false
+    love.audio.setEffect = false
+    love.audio.setMaxSceneEffects = false
+    love.audio.setMaxSourceEffects = false
+    love.audio.setOrientation = false
+    love.audio.setPosition = false
+    love.audio.setRecordingDevices = false
+    love.audio.setVelocity = false
+    love.audio.setVolume = false
+
+    -- sound --
+
+    love.sound.newDecoder = false
 end
-
-
 
 return nativelocks
